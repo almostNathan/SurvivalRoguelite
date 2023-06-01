80431d3d7f0b40ae2c0c194d017b55290997d124
Nathan Allen <nallen@indybbb.org>
2023-06-01 09:52:43 -0400

    continuing upgrades

--- Pickups/Upgrades/upgrade.gd
+++ Pickups/Upgrades/upgrade.gd
@@ -4,10 +4,18 @@ var upgrade_loot : Upgrade
 
 func _ready():
 	upgrade_loot = Upgrade.new()
-	upgrade_loot.size = 3
+	set_upgrade_stats()
+
 	
 func take_upgrade():
 	if is_lootable == true:
 		is_lootable = false
 		return upgrade_loot
 		
+
+func set_upgrade_stats():
+	upgrade_loot.damage_mult = 1
+	upgrade_loot.speed
+	upgrade_loot.bounce
+	upgrade_loot.projectile
+	upgrade_loot.size
--- Scripts/Upgrade.gd
+++ Scripts/Upgrade.gd
@@ -1,7 +1,15 @@
 class_name Upgrade
 
-var damage = 1
-var speed = 1
-var bounce = 0
-var projectile = 0
-var size
+
+var damage_mult
+var size_mult
+var speed_mult
+var bounce_cnt
+var projectile_cnt
+
+func set_base_stats():
+	damage_mult = 1
+	speed_mult = 1
+	size_mult = 1
+	projectile_cnt = 0
+	bounce_cnt = 0
