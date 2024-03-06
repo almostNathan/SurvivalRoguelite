0f6db33d4d1152c2d8a4364b4e42ec46b847add4
Nathan Allen <nallen@iabbb.org>
2024-03-06 15:53:46 -0500

    UI implemented, working build

--- Scripts/Health.gd
+++ Scripts/Health.gd
@@ -9,13 +9,15 @@ func take_damage(value):
 		cur_health = 0
 	elif cur_health >= max_health:
 		cur_health = max_health
-		
+	update_hud()
+
 func lose_life(value):
 	cur_health -= value
 	if cur_health <= 0:
 		cur_health = 0
 	elif cur_health >= max_health:
 		cur_health = max_health
+	update_hud()
 
 func gain_life(value):
 	cur_health += value
@@ -23,3 +25,8 @@ func gain_life(value):
 		cur_health = 0
 	elif cur_health >= max_health:
 		cur_health = max_health
+	update_hud()
+
+func update_hud():
+	Hud.health_bar.value = cur_health
+	Hud.health_bar.max_value = max_health
